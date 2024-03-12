3610785714068c7bea4682717abc126b51fe2c1c
Ugisele <Umutonigisele12@gmail.com>
2024-03-12 11:35:37 +0200

    Welcome,GetStarted and Button component create

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Started from './components/GetStarted';
+import Welcome from './components/Welcome';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View >
+
+      <Started/>
+      {/* <Welcome/> */}
+
       <StatusBar style="auto" />
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
