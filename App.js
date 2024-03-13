cef7d22c4ee70b8e4c8f3844638d243d272ab3f5
DeliceLydia <Delydia84@gmail.com>
2024-03-13 09:40:33 +0200

    Add authentication

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import Started from './components/GetStarted';
+import Login from './components/Login';
+import Register from './components/Register';
 import Welcome from './components/Welcome';
 
 export default function App() {
   return (
     <View >
 
-      <Started/>
+      {/* <Started/> */}
       {/* <Welcome/> */}
-
+      <Register />
+      {/* <Login /> */}
       <StatusBar style="auto" />
 
     </View>
