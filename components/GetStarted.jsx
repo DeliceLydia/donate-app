a3f3a5a9103d8cf144675b50bdfe84336a0bf706
lenlanaa <tracuna005@gmail.com>
2024-03-14 12:55:25 +0200

    Add fundraising component

--- components/GetStarted.jsx
+++ components/GetStarted.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Image, Text, Pressable, View, Dimensions } from "react-native";
 import Button from "./Button";
 import { Image, Text, TouchableOpacity, View, Dimensions } from "react-native";
 
--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -4,18 +4,20 @@ import Welcome from "./Welcome";
 import Login from './Login';
 import Register from './Register';
 import Congz from './Congratulation';
+import Fundraising from "./Fundraising";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
 export const StackNav = () => {
   return (
-    <Stack.Navigator initialRouteName="start">
+    <Stack.Navigator initialRouteName="fundarisation">
       <Stack.Screen name="start" component={Started} options={{ headerShown: false }}/>
       <Stack.Screen name="welcome" component={Welcome} />
       <Stack.Screen name="login" component={Login} />
       <Stack.Screen name="register" component={Register} />
       <Stack.Screen name="congratulation" component={Congz} />
+      <Stack.Screen name="fundarisation" component={Fundraising} options={{ headerShown: false }}/>
     </Stack.Navigator>
   );
 };
