f339464ddcbe16b92e5efb2e6a522c013d4cc71b
Lydia Ingabire <Delydia84@gmail.com>
2024-03-15 11:00:39 +0200

    Add fundraising file (#8)

---
+++ components/ComingToEnd.jsx
@@ -0,0 +1,135 @@
+import { View, Image, Text, Pressable, StyleSheet, ScrollView } from "react-native";
+import { TextInput } from "react-native-paper";
+import DonationTypes from "./DonationTypes";
+
+const ComingToAnEnd = () => {
+  return (
+    <View>
+      <View style={{paddingHorizontal: 20, paddingVertical: 10}}>
+        <TextInput
+          theme={{
+            colors: {
+              primary: "#15b257",
+            },
+          }}
+          style={styles.input}
+          width= '100%'
+          mode="flat"
+          backgroundColor= '#f4f7fa'
+          placeholder="Search"
+          placeholderTextColor="#a6aaaf"
+          color="#595b5f"
+          paddingLeft={10}
+          underlineColor="#f4f6f9"
+          textColor="#868889"
+          right={
+            <TextInput.Icon
+              icon="magnify"
+              color="#b6b9be"
+              backgroundColor="#f4f6f9"
+            />
+          }
+        />
+      </View>
+      <DonationTypes />
+<ScrollView>
+<View style={styles.card}>
+        <Image
+          source={{
+            uri: `https://media.istockphoto.com/id/538570416/photo/poor-indian-family-on-the-street-in-allahabad-india.jpg?s=612x612&w=0&k=20&c=QNKeBwf1_YIE_d9MYwoySEZ9VZq-wa1MTsy0QEizwFY=`,
+          }}
+          style={styles.cardImage}
+        />
+        <View style={{marginTop: 20}}>
+          <Text style={{color: '#000', fontWeight: 'bold', marginBottom: 5}}>Help overcome malnutrition</Text>
+          <Text>
+            <Text style={{color: '#6bd19b'}}>$8755 </Text>
+            <Text>Funds raised from $10540</Text>
+          </Text>
+        </View>
+      </View>
+      <View style={styles.card}>
+        <Image
+          source={{
+            uri: `https://www.shutterstock.com/image-photo/happy-african-male-doctor-examining-600nw-133030739.jpg`,
+          }}
+          style={styles.cardImage}
+        />
+        <View style={{marginTop: 20}}>
+          <Text style={{color: '#000', fontWeight: 'bold', marginBottom: 5}}>Help Improve Child Health</Text>
+          <Text>
+            <Text style={{color: '#6bd19b'}}>$2277 </Text>
+            <Text>Funds raised from $6310</Text>
+          </Text>
+        </View>
+      </View>
+      <View style={styles.card}>
+        <Image
+          source={{
+            uri: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4m4eLKFNjt3MSvij7ZUTdrAJf3dJ5CXeSmQ&usqp=CAU`,
+          }}
+          style={styles.cardImage}
+        />
+        <View style={{marginTop: 20}}>
+          <Text style={{color: '#000', fontWeight: 'bold', marginBottom: 5}}>Help Improve Healthy Food</Text>
+          <Text>
+            <Text style={{color: '#6bd19b'}}>$2277 </Text>
+            <Text>Funds raised from $6310</Text>
+          </Text>
+        </View>
+      </View>
+      <View style={styles.card}>
+        <Image
+          source={{
+            uri: `https://www.shutterstock.com/image-photo/happy-african-male-doctor-examining-600nw-133030739.jpg`,
+          }}
+          style={styles.cardImage}
+        />
+        <View style={{marginTop: 20}}>
+          <Text style={{color: '#000', fontWeight: 'bold', marginBottom: 5}}>Help Overcome Malnutrition</Text>
+          <Text>
+            <Text style={{color: '#6bd19b'}}>$2277 </Text>
+            <Text>Funds raised from $6310</Text>
+          </Text>
+        </View>
+      </View>
+      <View style={styles.card}>
+        <Image
+          source={{
+            uri: `https://www.shutterstock.com/image-photo/happy-african-male-doctor-examining-600nw-133030739.jpg`,
+          }}
+          style={styles.cardImage}
+        />
+        <View style={{marginTop: 20}}>
+          <Text style={{color: '#000', fontWeight: 'bold', marginBottom: 5}}>Help Improve Child Health</Text>
+          <Text>
+            <Text style={{color: '#6bd19b'}}>$2277 </Text>
+            <Text>Funds raised from $6310</Text>
+          </Text>
+        </View>
+      </View>
+</ScrollView>
+
+      
+    </View>
+  );
+};
+
+export default ComingToAnEnd;
+
+const styles = StyleSheet.create({
+  card: {
+    width: 380,
+    marginTop: 20,
+    marginLeft: 5,
+    flexDirection: "row",
+    borderWidth: 2,
+    borderColor: '#f7f9fc',
+    gap: 10,
+  },
+  cardImage: {
+    width: "40%",
+    height: 120,
+    resizeMode: "cover",
+  }
+});
--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -4,6 +4,8 @@ import Welcome from "./Welcome";
 import Login from './Login';
 import Register from './Register';
 import Congz from './Congratulation';
+import ComingToAnEnd from "./ComingToEnd";
+import Icon from "react-native-vector-icons/FontAwesome";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ForgetPW from "./ForgetPW";
 
@@ -11,13 +13,24 @@ const Stack = createNativeStackNavigator();
 
 export const StackNav = () => {
   return (
-    <Stack.Navigator initialRouteName="start">
+    <Stack.Navigator initialRouteName="login">
       <Stack.Screen name="start" component={Started} options={{ headerShown: false }}/>
-      <Stack.Screen name="welcome" component={Welcome} options={{ headerShown: false }} />
-      <Stack.Screen name="login" component={Login} options={{ headerShown: false }} />
-      <Stack.Screen name="register" component={Register}  options={{ headerShown: false }}/>
+      <Stack.Screen name="welcome" component={Welcome} />
+      <Stack.Screen name="login" component={Login} />
+      <Stack.Screen name="register" component={Register} />
+      <Stack.Screen name="congratulation" component={Congz} />
+      <Stack.Screen name="Dashboard" component={ComingToAnEnd} options={{
+          headerLeft: () => (
+            <Icon
+              name="arrow-left"
+              size={20}
+              color="#15b257"
+              style={{ marginLeft: 10, marginRight: 10 }}
+            />
+          ),
+        }}
+        />
       <Stack.Screen name="forget" component={ForgetPW}  options={{ headerShown: false }}/>
-      <Stack.Screen name="congratulation" component={Congz}  options={{ headerShown: false }}/>
     </Stack.Navigator>
   );
 };
