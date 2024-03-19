2677c381d0785fb3a822eb7f59bec76980cb05e2
Ugisele <Umutonigisele12@gmail.com>
2024-03-19 17:00:05 +0200

    Card component

--- components/Search.jsx
+++ components/Search.jsx
@@ -70,8 +70,6 @@ const Search = () => {
                         />
                     </TouchableOpacity>
                 )}
-                
-                
                 />
             </View>
             <StatusBar />
